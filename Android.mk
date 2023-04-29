003ad26baf77ad2dc4d7ff73b98533ab6ba9a4f2
me-cafebabe <me.cafebabe@gmail.com>
2023-04-30 00:00:09 +0800

    prada: camera: Guard for Mi8937 only

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(USES_DEVICE_XIAOMI_MI8937),true)
+
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8916
@@ -23,4 +25,6 @@ ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
       include $(call all-subdir-makefiles)
     endif
   endif
+endif
+
 endif
