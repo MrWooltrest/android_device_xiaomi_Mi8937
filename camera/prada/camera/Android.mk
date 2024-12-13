40904830958a5e9981a0c5a24ca4ec2d6089e2bf
MrWooltrest <wooltrest@gmail.com>
2024-12-13 10:04:54 +0200

    prada: camera: Move USES_DEVICE_XIAOMI_MI8937

--- camera/prada/camera/Android.mk
+++ camera/prada/camera/Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(USES_DEVICE_XIAOMI_MI8937),true)
-
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8916
@@ -25,6 +23,4 @@ ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
       include $(call all-subdir-makefiles)
     endif
   endif
-endif
-
 endif
